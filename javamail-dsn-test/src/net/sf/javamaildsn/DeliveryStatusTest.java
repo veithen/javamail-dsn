93b2164007e7f3f4e2d74417949301453b58db3a
veithen <veithen@ef5f619d-162f-0410-a2ce-98492f3436ce>
2007-04-27 20:17:04 +0000

    Added test for multiline SMTP diagnostics.
    git-svn-id: https://svn.code.sf.net/p/javamail-dsn/code/trunk@13 ef5f619d-162f-0410-a2ce-98492f3436ce

---
+++ javamail-dsn-test/src/net/sf/javamaildsn/DeliveryStatusTest.java
@@ -0,0 +1,29 @@
+package net.sf.javamaildsn;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+
+import net.sf.javamaildsn.PerRecipientDeliveryStatus.Action;
+
+import junit.framework.TestCase;
+
+public class DeliveryStatusTest extends TestCase {
+	public void testMultilineSmtpDiagnostic() throws MessagingException {
+		DeliveryStatus ds = new DeliveryStatus(getClass().getResourceAsStream("/multiline-smtp-diagnostic"));
+		PerRecipientDeliveryStatus[] rdsArray = ds.getPerRecipientParts();
+		assertEquals(1, rdsArray.length);
+		PerRecipientDeliveryStatus rds = rdsArray[0];
+		assertEquals(new InternetAddress("michelle.konzack@freenet.de"), rds.getFinalRecipient());
+		assertEquals(Action.FAILED, rds.getAction());
+		assertEquals(new MailSystemStatus(5, 0, 0), rds.getStatus());
+		Diagnostic diagnostic = rds.getDiagnostic();
+		assertEquals(SMTPDiagnostic.class, diagnostic.getClass());
+		SMTPDiagnostic smtpDiagnostic = (SMTPDiagnostic)diagnostic;
+		assertEquals(550, smtpDiagnostic.getCode());
+		String[] messages = smtpDiagnostic.getMessages();
+		assertEquals(3, messages.length);
+		assertEquals("Verification failed for <root@tamay-dogan.net>", messages[0]);
+		assertEquals("unrouteable mail domain \"tamay-dogan.net\"", messages[1]);
+		assertEquals("verifying root@tamay-dogan.net failed", messages[2]);
+	}
+}
