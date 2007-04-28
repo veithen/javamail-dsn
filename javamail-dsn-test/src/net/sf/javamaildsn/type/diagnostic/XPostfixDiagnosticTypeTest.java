51b5b2a37b6b555fa290fa448b26c484e7865fa3
veithen <veithen@ef5f619d-162f-0410-a2ce-98492f3436ce>
2007-04-28 22:30:03 +0000

    Improved parsing of typed DSN fields.
    git-svn-id: https://svn.code.sf.net/p/javamail-dsn/code/trunk@19 ef5f619d-162f-0410-a2ce-98492f3436ce

--- javamail-dsn-test/src/net/sf/javamaildsn/type/diagnostic/XPostfixDiagnosticTypeTest.java
+++ javamail-dsn-test/src/net/sf/javamaildsn/type/diagnostic/XPostfixDiagnosticTypeTest.java
@@ -1,10 +1,18 @@
-package net.sf.javamaildsn;
+package net.sf.javamaildsn.type.diagnostic;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeUtility;
 
 import junit.framework.TestCase;
+import net.sf.javamaildsn.Diagnostic;
+import net.sf.javamaildsn.SMTPDiagnostic;
+import net.sf.javamaildsn.XPostfixRelayDiagnostic;
 
-public class XPostfixDiagnosticParserTest extends TestCase {
-	public void testRelayDiagnostic() {
-		Diagnostic diagnostic = XPostfixDiagnosticParser.parse("host mx.example.com[10.0.0.1] said: 550 Error: Message content rejected");
+public class XPostfixDiagnosticTypeTest extends TestCase {
+	private final XPostfixDiagnosticType type = new XPostfixDiagnosticType();
+	
+	public void testRelayDiagnostic() throws MessagingException {
+		Diagnostic diagnostic = type.parse(null, "host mx.example.com[10.0.0.1] said: 550 Error: Message content rejected");
 		assertNotNull(diagnostic);
 		assertEquals(XPostfixRelayDiagnostic.class, diagnostic.getClass());
 		XPostfixRelayDiagnostic relayDiagnostic = (XPostfixRelayDiagnostic)diagnostic;
@@ -17,11 +25,11 @@ public class XPostfixDiagnosticParserTest extends TestCase {
 		assertEquals("Error: Message content rejected", messages[0]);
 	}
 	
-	public void testFoldedLMTPRelayDiagnostic() {
-		Diagnostic diagnostic = XPostfixDiagnosticParser.parse(
+	public void testFoldedLMTPRelayDiagnostic() throws MessagingException {
+		Diagnostic diagnostic = type.parse(null, MimeUtility.unfold(
 			"host /var/imap/socket/lmtp[/var/imap/socket/lmtp]\n" +
 			"\tsaid: 554 5.6.0 Message contains NUL characters (in reply to end of DATA\n" +
-			"\tcommand)");
+			"\tcommand)"));
 		assertNotNull(diagnostic);
 		assertEquals(XPostfixRelayDiagnostic.class, diagnostic.getClass());
 		XPostfixRelayDiagnostic relayDiagnostic = (XPostfixRelayDiagnostic)diagnostic;
@@ -33,12 +41,12 @@ public class XPostfixDiagnosticParserTest extends TestCase {
 		assertEquals("end of DATA command", relayDiagnostic.getInReplyTo());
 	}
 	
-	public void testMultilineRelayDiagnostic() {
-		Diagnostic diagnostic = XPostfixDiagnosticParser.parse(
+	public void testMultilineRelayDiagnostic() throws MessagingException {
+		Diagnostic diagnostic = type.parse(null, MimeUtility.unfold(
 			"host 127.0.0.1[127.0.0.1] said: 550-Mailbox\n" +
 			"\tunknown.  Either there is no mailbox associated with this 550-name or you\n" +
 			"\tdo not have authorization to see it. 550 5.1.1 User unknown (in reply to\n" +
-			"\tRCPT TO command)");
+			"\tRCPT TO command)"));
 		assertNotNull(diagnostic);
 		assertEquals(XPostfixRelayDiagnostic.class, diagnostic.getClass());
 		XPostfixRelayDiagnostic relayDiagnostic = (XPostfixRelayDiagnostic)diagnostic;
