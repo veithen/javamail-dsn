1a612be33a11c6cdbbc83cb1ae73a3a1ba49f577
veithen <veithen@ef5f619d-162f-0410-a2ce-98492f3436ce>
2007-04-29 09:59:33 +0000

    Improved parsing of "dns" MTA names.
    git-svn-id: https://svn.code.sf.net/p/javamail-dsn/code/trunk@31 ef5f619d-162f-0410-a2ce-98492f3436ce

--- javamail-dsn/src/net/sf/javamaildsn/type/mtaname/DnsMtaNameType.java
+++ javamail-dsn/src/net/sf/javamaildsn/type/mtaname/DnsMtaNameType.java
@@ -11,32 +11,51 @@ import net.sf.javamaildsn.DnsMtaName;
 import net.sf.javamaildsn.type.FieldType;
 
 /**
+ * 
+ * Section 9.3 of RFC 3461 specifies the syntax as follows:
+ * <pre>
+ * domain = real-domain / domain-literal
+ * real-domain = sub-domain *("." sub-domain)
+ * sub-domain = atom
+ * domain-literal = "[" 1*3DIGIT 3("." 1*3DIGIT) "]"
+ * </pre>
+ * Section 2.2.4 of RFC 3464 also suggests using a network address as
+ * comment. This is supported. Any other comments are discarded.
+ * <p>
+ * Some MTAs also use the following syntax:
+ * <pre>
+ * real-domain SP domain-literal
+ * </pre>
+ * Even if this is not in accordance with the RFCs, this syntax is supported.
+ * 
  * @author Andreas Veithen
  */
 public class DnsMtaNameType implements FieldType<DnsMtaName> {
-	private final static Pattern valuePattern = Pattern.compile("([^\\s]*)(\\s\\((.*)\\))?");
-	private final static Pattern domainLiteralPattern = Pattern.compile("\\[(\\d{1,3}(\\.\\d{1,3}){3})\\]");
-	private final static Pattern addressPattern = Pattern.compile("\\d{1,3}(\\.\\d{1,3}){3}");
+	private final static Pattern atomPattern = Pattern.compile("[\\x21-\\x7F&&[^\\(\\)<>@,;:\\\\\"\\.\\[\\]]]+");
+	private final static Pattern addressPattern = Pattern.compile("\\d{1,3}(?:\\.\\d{1,3}){3}");
+	private final static Pattern domainNamePattern = Pattern.compile(atomPattern + "(?:\\." + atomPattern + ")*");
+	private final static Pattern domainLiteralPattern = Pattern.compile("\\[(" + addressPattern + ")\\]");
+	private final static Pattern valuePattern = Pattern.compile("(?:(" + domainNamePattern + ")|(?:" + domainLiteralPattern + "))(?:\\s\\((.*)\\))?");
+	private final static Pattern altValuePattern = Pattern.compile("(" + domainNamePattern + ")\\s(?:" + domainLiteralPattern + ")");
 	
 	public DnsMtaName parse(String type, String value) throws MessagingException {
-		Matcher matcher = valuePattern.matcher(value);
-		if (!matcher.matches()) {
-			throw new MessagingException("Invalid dns MTA name format in '" + value + "'");
-		}
 		String domainName;
 		String address;
-		String mtaName = matcher.group(1);
-		String comment = matcher.group(3);
-		matcher = domainLiteralPattern.matcher(mtaName);
+		Matcher matcher = valuePattern.matcher(value);
 		if (matcher.matches()) {
-			domainName = null;
-			address = matcher.group(1);
-		} else {
-			domainName = mtaName;
-			if (comment != null && addressPattern.matcher(comment).matches()) {
+			domainName = matcher.group(1);
+			address = matcher.group(2);
+			String comment = matcher.group(3);
+			if (comment != null && address == null && addressPattern.matcher(comment).matches()) {
 				address = comment;
+			}
+		} else {
+			matcher = altValuePattern.matcher(value);
+			if (matcher.matches()) {
+				domainName = matcher.group(1);
+				address = matcher.group(2);
 			} else {
-				address = null;
+				throw new MessagingException("Invalid dns MTA name format in '" + value + "'");
 			}
 		}
 		try {
