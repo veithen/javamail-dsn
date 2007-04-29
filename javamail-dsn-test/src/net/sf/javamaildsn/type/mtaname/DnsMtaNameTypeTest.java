69672280beb79b2009dd2db0db6032e9c1c5f64d
veithen <veithen@ef5f619d-162f-0410-a2ce-98492f3436ce>
2007-04-29 10:17:32 +0000

    Added toString method to DnsMtaName.
    git-svn-id: https://svn.code.sf.net/p/javamail-dsn/code/trunk@32 ef5f619d-162f-0410-a2ce-98492f3436ce

--- javamail-dsn-test/src/net/sf/javamaildsn/type/mtaname/DnsMtaNameTypeTest.java
+++ javamail-dsn-test/src/net/sf/javamaildsn/type/mtaname/DnsMtaNameTypeTest.java
@@ -15,29 +15,34 @@ public class DnsMtaNameTypeTest extends TestCase {
 		DnsMtaName mtaName = type.parse("dns", "mx.example.com");
 		assertEquals("mx.example.com", mtaName.getDomainName());
 		assertNull(mtaName.getAddress());
+		assertEquals("mx.example.com", mtaName.toString());
 	}
 
 	public void testDomainLiteral() throws Exception {
 		DnsMtaName mtaName = type.parse("dns", "[192.168.2.45]");
 		assertNull(mtaName.getDomainName());
 		assertEquals(InetAddress.getByName("192.168.2.45"), mtaName.getAddress());
+		assertEquals("[192.168.2.45]", mtaName.toString());
 	}
 	
 	public void testDomainNameWithAddressComment() throws Exception {
 		DnsMtaName mtaName = type.parse("dns", "mx.example.com (192.168.2.45)");
 		assertEquals("mx.example.com", mtaName.getDomainName());
 		assertEquals(InetAddress.getByName("192.168.2.45"), mtaName.getAddress());
+		assertEquals("mx.example.com (192.168.2.45)", mtaName.toString());
 	}
 	
 	public void testDomainNameWithOtherComment() throws Exception {
 		DnsMtaName mtaName = type.parse("dns", "mx.example.com (some other comment)");
 		assertEquals("mx.example.com", mtaName.getDomainName());
 		assertEquals(null, mtaName.getAddress());
+		assertEquals("mx.example.com", mtaName.toString());
 	}
 	
 	public void testDomainNameWithDomainLiteral() throws Exception {
 		DnsMtaName mtaName = type.parse("dns", "mx.example.com [192.168.2.45]");
 		assertEquals("mx.example.com", mtaName.getDomainName());
 		assertEquals(InetAddress.getByName("192.168.2.45"), mtaName.getAddress());
+		assertEquals("mx.example.com (192.168.2.45)", mtaName.toString());
 	}
 }
