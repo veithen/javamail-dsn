236ae4eb9e509d5cf770d3cfbe593b144bed8ab3
veithen <veithen@ef5f619d-162f-0410-a2ce-98492f3436ce>
2007-04-29 09:22:22 +0000

    Corrected SMTP diagnostic parsing.
    git-svn-id: https://svn.code.sf.net/p/javamail-dsn/code/trunk@28 ef5f619d-162f-0410-a2ce-98492f3436ce

--- javamail-dsn/src/net/sf/javamaildsn/type/diagnostic/SMTPDiagnosticType.java
+++ javamail-dsn/src/net/sf/javamaildsn/type/diagnostic/SMTPDiagnosticType.java
@@ -1,12 +1,7 @@
 package net.sf.javamaildsn.type.diagnostic;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.StringReader;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javax.mail.MessagingException;
 
@@ -17,49 +12,48 @@ import net.sf.javamaildsn.type.FieldType;
  * @author Andreas Veithen
  */
 public class SMTPDiagnosticType implements FieldType<SMTPDiagnostic> {
-	private final static Pattern linePattern = Pattern.compile("\\s*(\\d{3})[ -](.*)");
-	
 	public SMTPDiagnostic parse(String type, String value) throws MessagingException {
-		int code = -1;
+		if (value.length() < 4) {
+			throw new MessagingException("Invalid SMTP diagnostic format");
+		}
 		List<String> messages = new LinkedList<String>();
-		BufferedReader in = new BufferedReader(new StringReader(value));
-		String line;
-		StringBuilder buffer = new StringBuilder();
-		do {
-			try {
-				line = in.readLine();
-			}
-			catch (IOException ex) {
-				// Reading from a StringReader should never cause an IOException
-				throw new Error(ex);
-			}
-			boolean flush; // True if the current content of the buffer should be flushed
-			String chunk; // The piece of message text recovered in the current iteration
-			if (line == null) {
-				flush = true;
-				chunk = null;
+		String codeString = value.substring(0, 3);
+		int code;
+		boolean hasMoreLines;
+		try {
+			code = Integer.parseInt(codeString);
+		}
+		catch (NumberFormatException ex) {
+			throw new MessagingException("Invalid SMTP diagnostic format: expected SMTP reply code");
+		}
+		switch (value.charAt(3)) {
+			case '-': hasMoreLines = true; break;
+			case ' ': hasMoreLines = false; break;
+			default: throw new MessagingException("Invalid SMTP diagnostic format: expected '-' or space after SMTP reply code");
+		}
+		int index = 4;
+		while (true) {
+			if (hasMoreLines) {
+				loop: while (true) {
+					int newIndex = value.indexOf(codeString, index);
+					if (newIndex == -1) {
+						throw new MessagingException("Invalid SMTP diagnostic format");
+					} else {
+						switch (value.charAt(newIndex+3)) {
+							case ' ':
+								hasMoreLines = false;
+							case '-':
+								messages.add(value.substring(index, newIndex).trim());
+								index = newIndex+4;
+								break loop;
+						}
+					}
+				}
 			} else {
-				Matcher matcher = linePattern.matcher(line);
-				if (matcher.matches()) {
-					code = Integer.valueOf(matcher.group(1));
-					chunk = matcher.group(2);
-					flush = true;
-				} else {
-					chunk = line;
-					flush = false;
-				}
+				messages.add(value.substring(index));
+				break;
 			}
-			if (flush && (buffer.length() > 0)) {
-				messages.add(buffer.toString());
-				buffer.setLength(0);
-			}
-			if (chunk != null) {
-				if (buffer.length() > 0) {
-					buffer.append(' ');
-				}
-				buffer.append(chunk);
-			}
-		} while (line != null);
+		}
 		return new SMTPDiagnostic(code, messages.toArray(new String[messages.size()]));
 	}
 }
