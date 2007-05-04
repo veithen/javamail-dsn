ab9056a354dcef152de5214e95336fe19304ac88
veithen <veithen@ef5f619d-162f-0410-a2ce-98492f3436ce>
2007-05-04 22:44:20 +0000

    Added test cases for default heuristics.
    git-svn-id: https://svn.code.sf.net/p/javamail-dsn/code/trunk@53 ef5f619d-162f-0410-a2ce-98492f3436ce

---
+++ javamail-dsn-test/src/net/sf/javamaildsn/heuristics/RuleBasedHeuristicsTest.java
@@ -0,0 +1,26 @@
+package net.sf.javamaildsn.heuristics;
+
+import java.io.InputStream;
+
+import junit.framework.TestCase;
+import net.sf.javamaildsn.DeliveryStatus;
+
+import org.apache.commons.vfs.FileObject;
+import org.apache.commons.vfs.FileSystemManager;
+import org.apache.commons.vfs.VFS;
+
+public class RuleBasedHeuristicsTest extends TestCase {
+	public void testDefault() throws Exception {
+		Heuristics heuristics = RuleBasedHeuristics.getDefaultInstance();
+		FileSystemManager fsm = VFS.getManager();
+		FileObject root = fsm.resolveFile(RuleBasedHeuristicsTest.class.getResource("/heuristics/.root").toExternalForm()).getParent();
+		for (Reason reason : Reason.values()) {
+			for (FileObject file : root.resolveFile(reason.toString().toLowerCase()).getChildren()) {
+				InputStream is = file.getContent().getInputStream();
+				DeliveryStatus ds = new DeliveryStatus(is);
+				is.close();
+				assertEquals(reason, heuristics.getReason(ds.getPerRecipientParts()[0].getDiagnostic()));
+			}
+		}
+	}
+}
