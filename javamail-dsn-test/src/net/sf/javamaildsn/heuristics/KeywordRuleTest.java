ea49a6eaa9fae74ab44efdd758e420a263fb0a4f
veithen <veithen@ef5f619d-162f-0410-a2ce-98492f3436ce>
2007-05-06 09:51:56 +0000

    Added test case for KeywordRule.
    git-svn-id: https://svn.code.sf.net/p/javamail-dsn/code/trunk@58 ef5f619d-162f-0410-a2ce-98492f3436ce

---
+++ javamail-dsn-test/src/net/sf/javamaildsn/heuristics/KeywordRuleTest.java
@@ -0,0 +1,27 @@
+package net.sf.javamaildsn.heuristics;
+
+import junit.framework.TestCase;
+
+public class KeywordRuleTest extends TestCase {
+	private final BoundaryMatcher boundaryMatcher = new BoundaryMatcher(new char[] { ' ' });
+	
+	public void test1() {
+		String normalizedMessage = RuleBasedHeuristics.normalizeMessage("xxx match xxx");
+		assertTrue(new KeywordRule("match", boundaryMatcher).matches(-1, null, normalizedMessage));
+	}
+
+	public void test2() {
+		String normalizedMessage = RuleBasedHeuristics.normalizeMessage("xxx yyy xxx");
+		assertFalse(new KeywordRule("match", boundaryMatcher).matches(-1, null, normalizedMessage));
+	}
+
+	public void test3() {
+		String normalizedMessage = RuleBasedHeuristics.normalizeMessage("xxx nomatch xxx");
+		assertFalse(new KeywordRule("match", boundaryMatcher).matches(-1, null, normalizedMessage));
+	}
+	
+	public void test4() {
+		String normalizedMessage = RuleBasedHeuristics.normalizeMessage("xxx nomatch match xxx");
+		assertTrue(new KeywordRule("match", boundaryMatcher).matches(-1, null, normalizedMessage));
+	}
+}
